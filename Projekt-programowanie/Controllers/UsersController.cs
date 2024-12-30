f3888cac5551e4ed6a5af5de4cdcea948af33802
Sebastian Kozłowski <sebastiankozlowski84@gmail.com>
2024-12-30 20:15:28 +0100

    refactor: Change auth system

--- Projekt-programowanie/Controllers/UsersController.cs
+++ Projekt-programowanie/Controllers/UsersController.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using ProjektProgramowanie.Models;
-using System.Threading.Tasks;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProjektProgramowanie.Controllers
 {
-	[Authorize(Roles = "admin, employee")]
 	public class UsersController : Controller
 	{
 		private readonly UserManager<ApplicationUser> _userManager;
@@ -21,6 +19,7 @@ namespace ProjektProgramowanie.Controllers
 		public async Task<IActionResult> Index()
 		{
 			var users = _userManager.Users.ToList();
+
 			var userRoles = new Dictionary<string, IList<string>>();
 
 			foreach (var user in users)
@@ -29,110 +28,27 @@ namespace ProjektProgramowanie.Controllers
 				userRoles[user.Id] = roles;
 			}
 
-			var model = new UserListViewModel
+			var viewModel = new UserListViewModel
 			{
 				Users = users,
 				UserRoles = userRoles
 			};
 
-			return View(model);
+			return View(viewModel);
 		}
-
-		public async Task<IActionResult> Edit(string id)
+		public async Task<IActionResult> Details(string id)
 		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
 			var user = await _userManager.FindByIdAsync(id);
 			if (user == null)
 			{
 				return NotFound();
 			}
 
-			var model = new EditUserViewModel
-			{
-				Id = user.Id,
-				UserName = user.UserName,
-				Email = user.Email,
-				FirstName = user.FirstName,
-				LastName = user.LastName,
-				Adress = user.Adress,
-				PhoneNumber = user.PhoneNumber
-			};
-
-			return View(model);
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> Edit(EditUserViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userManager.FindByIdAsync(model.Id);
-				if (user == null)
-				{
-					return NotFound();
-				}
-
-				user.UserName = model.UserName;
-				user.Email = model.Email;
-				user.FirstName = model.FirstName;
-				user.LastName = model.LastName;
-				user.Adress = model.Adress;
-				user.PhoneNumber = model.PhoneNumber;
-
-				var result = await _userManager.UpdateAsync(user);
-				if (result.Succeeded)
-				{
-					if (!string.IsNullOrEmpty(model.Password))
-					{
-						var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-						var passwordResult = await _userManager.ResetPasswordAsync(user, token, model.Password);
-						if (!passwordResult.Succeeded)
-						{
-							foreach (var error in passwordResult.Errors)
-							{
-								ModelState.AddModelError(string.Empty, error.Description);
-							}
-							return View(model);
-						}
-					}
-					return RedirectToAction(nameof(Index));
-				}
-				foreach (var error in result.Errors)
-				{
-					ModelState.AddModelError(string.Empty, error.Description);
-				}
-			}
-			return View(model);
-		}
-		[Authorize(Roles = "admin")]
-		public async Task<IActionResult> Delete(string id)
-		{
-			var user = await _userManager.FindByIdAsync(id);
-			if (user == null)
-			{
-				return NotFound();
-			}
-			return View(user);
-		}
-		
-		[HttpPost, ActionName("Delete")]
-		[Authorize(Roles = "admin")]
-		public async Task<IActionResult> DeleteConfirmed(string id)
-		{
-			var user = await _userManager.FindByIdAsync(id);
-			if (user == null)
-			{
-				return NotFound();
-			}
-
-			var result = await _userManager.DeleteAsync(user);
-			if (result.Succeeded)
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			foreach (var error in result.Errors)
-			{
-				ModelState.AddModelError(string.Empty, error.Description);
-			}
 			return View(user);
 		}
 	}
