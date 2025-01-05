129ce18af827fec1fb757f011cfb983e92793c2b
Sebastian Kozłowski <sebastiankozlowski84@gmail.com>
2025-01-05 19:08:43 +0100

    refactor: Delete UsersController, replace logic in AccountController

--- Projekt-programowanie/Controllers/HomeController.cs
+++ Projekt-programowanie/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProjektProgramowanie.Models;
 using System.Diagnostics;
@@ -7,10 +8,13 @@ namespace ProjektProgramowanie.Controllers
 {
 	[Authorize]
 	public class HomeController : Controller
+
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
 		}
@@ -30,5 +34,24 @@ namespace ProjektProgramowanie.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-	}
+
+        // using UserListViewModel from Models folder to display count of users and roles
+        public async Task<IActionResult> Users()
+        {
+            var users = _userManager.Users.ToList();
+            var userRoles = new Dictionary<string, IList<string>>();
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                userRoles[user.Id] = roles;
+            }
+            var viewModel = new UserListViewModel
+            {
+                Users = users,
+                UserRoles = userRoles
+            };
+            return View(viewModel);
+        }
+
+    }
 }
