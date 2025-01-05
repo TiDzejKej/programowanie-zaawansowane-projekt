177743a739768fe687acf1e334ef408abe009592
Sebastian Kozłowski <sebastiankozlowski84@gmail.com>
2025-01-05 19:10:18 +0100

    feat: Add UseAuthentication

--- Projekt-programowanie/Program.cs
+++ Projekt-programowanie/Program.cs
@@ -3,23 +3,22 @@ using Microsoft.EntityFrameworkCore;
 using ProjektProgramowanie.Data;
 using ProjektProgramowanie.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddRoles<IdentityRole>()
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.LoginPath = "/Account/Login"; 
+    options.LoginPath = "/Account/Login";
 });
 
 var app = builder.Build();
@@ -27,13 +26,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint();
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -41,64 +40,66 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "scheduler",
     pattern: "{controller=Scheduler}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-using(var scope = app.Services.CreateScope())
-{
-	var services = scope.ServiceProvider;
-	var context = services.GetRequiredService<ApplicationDbContext>();
-	var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-	var roles = new[] { "admin", "student", "lecturer", "employee" };
-
-	foreach (var role in roles)
-	{
-		if (!await roleManager.RoleExistsAsync(role))
-		{
-			await roleManager.CreateAsync(new IdentityRole(role));
-		}
-	}
-
-}
 using (var scope = app.Services.CreateScope())
 {
-	var services = scope.ServiceProvider;
-	var context = services.GetRequiredService<ApplicationDbContext>();
-	var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-	string email = "admin@admin.com";
-	string password = "qweASD1231410!";
+    var roles = new[] { "admin", "student", "lecturer", "employee" };
 
-	if (await userManager.FindByEmailAsync(email) == null)
-	{
-		var user = new ApplicationUser()
-		{
-			UserName = "admin",
-			Email = "admin@admin.com",
-			FirstName = "Admin",
-			LastName = "Admin",
-			Adress = "Admin",
-			PhoneNumber = "123456789",
-			CreatedAt = DateTime.Now,
-			EmailConfirmed = true
-		};
-		user.UserName = user.Email;
-		user.Email = user.Email;
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
 
-		await userManager.CreateAsync(user, password);
-		await userManager.AddToRoleAsync(user, "admin");
-		
-	}
+    // Ensure the database is created
+    context.Database.EnsureCreated();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+    string email = "admin@admin.com";
+    string password = "qweASD1231410!";
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new ApplicationUser()
+        {
+            UserName = "admin",
+            Email = "admin@admin.com",
+            FirstName = "Admin",
+            LastName = "Admin",
+            Adress = "Admin",
+            PhoneNumber = "123456789",
+            CreatedAt = DateTime.Now,
+            EmailConfirmed = true
+        };
+        user.UserName = user.Email;
+        user.Email = user.Email;
+
+        await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, "admin");
+    }
+}
 
 app.Run();
